b252493364801e5a622823a902bc5fa0be599c03
Udbhav Ashish Arya <143270660+Udbhav227@users.noreply.github.com>
2026-07-18 13:46:59 +0530

    Add space-optimized method for min subset sum difference
    Implemented space-optimized solution for finding minimum subset sum difference using a 1D array.

--- dynamic-programming/0-1 knapsack/min-subset-sum-difference.java
+++ dynamic-programming/0-1 knapsack/min-subset-sum-difference.java
@@ -44,3 +44,52 @@ public class SubsetSumDifference {
     }
   }
 }
+
+
+// Space Optimisation: using 1D arrya
+public static int findMinDifference(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        // We only need to find a subset sum closest to exactly half of the total sum
+        int target = totalSum / 2;
+
+        // dp[j] will store whether a subset sum of 'j' is possible
+        // Space Complexity: O(target) which is effectively O(Sum)
+        boolean[] dp = new boolean[target + 1];
+        
+        // Base Case: A sum of 0 is always possible using an empty subset
+        dp[0] = true; 
+
+        for (int num : nums) {
+            // CRITICAL: Iterate backwards from target down to the current number's value.
+            // This prevents using the same element multiple times in the same round.
+            for (int j = target; j >= num; j--) {
+                // If a subset sum of (j - num) was possible before, 
+                // adding 'num' makes a subset sum of 'j' possible now.
+                if (dp[j - num]) {
+                    dp[j] = true;
+                }
+            }
+        }
+
+        // Find the largest subset sum that is achievable and <= totalSum / 2
+        int closestSum = 0;
+        for (int j = target; j >= 0; j--) {
+            if (dp[j]) {
+                closestSum = j;
+                break; // Break at the very first 'true' since we are moving downwards from the max target
+            }
+        }
+
+        // Subset 1 sum = closestSum
+        // Subset 2 sum = totalSum - closestSum
+        // Difference = (totalSum - closestSum) - closestSum = totalSum - 2 * closestSum
+        return totalSum - (2 * closestSum);
+    }
